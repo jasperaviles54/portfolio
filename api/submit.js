8f1c0100238f6351518bc40a765abcbd11305ae2
jasperaviles54 <jasperaviles54@gmail.com>
2025-08-12 21:12:36 +0800

    update

--- api/submit.js
+++ api/submit.js
@@ -1,8 +1,5 @@
 export default function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "https://jasperaviles54.github.io");
-  res.setHeader("Access-Control-Allow-Origin", "portfolio-opal-zeta-50.vercel.app");
-  res.setHeader("Access-Control-Allow-Origin", "https://portfolio-git-main-jasper-aviles-projects.vercel.app");
-  res.setHeader("Access-Control-Allow-Origin", "https://portfolio-akrn7yrk0-jasper-aviles-projects.vercel.app");
+  res.setHeader("Access-Control-Allow-Origin", "https://jasperaviles54.github.io/portfolio");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   if (req.method === 'POST') {

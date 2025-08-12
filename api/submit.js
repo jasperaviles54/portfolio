7c88d05e613de0001fdcf002395123fc48ef0246
jasperaviles54 <jasperaviles54@gmail.com>
2025-08-13 01:02:20 +0800

    update

--- api/submit.js
+++ api/submit.js
@@ -2,6 +2,12 @@ export default function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', 'https://jasperaviles54.github.io');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   if (req.method === 'POST') {
     const { email, message } = req.body;
     console.log('Received:', email, message);
